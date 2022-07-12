c8a8ae7fd1456d36861b7dcee10810d993b33ec2
ワイくん <105576113+himazin355@users.noreply.github.com>
2022-07-12 20:40:56 +0900

    Create init.cpp

---
+++ init.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define CEIL(a,b) ((a) / (b) + ((a) % (b) == 0 ? 0 : 1))
+#define X first
+#define Y second
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vl = vector<ll>;
+using vs = vector<string>;
+
+constexpr int MOD = int(1e9 + 7);
+constexpr int INF = int(1e9 + 1);
+constexpr ll LLINF = ll(4 * 1e18 + 1);
+
+const int dx[] = {1, 0, -1, 0, 1, -1, -1, 1, 0};
+const int dy[] = {0, 1, 0, -1, 1, 1, -1, -1, 0};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(10);
+
+    return 0;
+}
